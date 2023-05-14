071a509e54a906646f6d20cbddaabb2d470969af
legion <88172317+ak14k@users.noreply.github.com>
2023-05-14 12:04:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "3447207"))
+API_HASH = environ.get("API_HASH", "19299e25620973ec31933a592c7c3592")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6123267912:AAF7FAB6Y7Mxa4g2KDDsyUZ845MpNPkS4tA")
+ADMIN = int(environ.get("ADMIN", "855478718"))          
+CAPTION = environ.get("CAPTION", "@Hemtai_Arena")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://telegra.ph/file/2527598b43a347979ba48.jpg")
